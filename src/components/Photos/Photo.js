2c88f3b7227ddb38bd999d2c1a63cc65837bc3c3
kennytran275 <kennyt275@gmail.com>
2021-03-18 21:04:53 -0400

    added a theme and styled-component

--- src/components/Photos/Photo.js
+++ src/components/Photos/Photo.js
@@ -1,18 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledPhoto = styled.div`
+  color: red;
+
+  h1 {
+    color: ${(prop) => prop.theme.primaryColor};
+  }
+`;
 
 const Photo = (props) => {
   const { photo } = props;
 
   return (
-    <div className="photo-border">
+    <StyledPhoto>
       <div className="photo-image-wrapper">
-        <img alt="photo thumbnail" className="photo-image" src={photo.url} />
+        <img alt="photo thumbnail" src={photo.url} />
       </div>
       <h1>{photo.title}</h1>
       <h2>{photo.copyright}</h2>
       <h3>{photo.date}</h3>
       <p>{photo.explanation}</p>
-    </div>
+    </StyledPhoto>
   );
 };
 
---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+// theme object here
+export default {
+  primaryColor: "royalBlue",
+  secondaryColor: "crimson",
+  tertiaryColor: "lime",
+  dangerColor: "red",
+  black: "black",
+  white: "white",
+  breakpointMobile: "(max-width: 550px)",
+  padding: {
+    small: "4px",
+    large: "12px",
+  },
+};
